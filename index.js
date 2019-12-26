748faf7bf3bb4a0f4558725caee0a2306fe9a8a4
Deekshith <deekshithchinna2@gmail.com>
2019-12-26 14:15:31 +0530

    updated

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT||3000
+const port = process.env.PORT || 3000
+app.use(express.static('public'))
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/start.html'))
-app.get('/resume', (req, res) => res.sendFile(__dirname + '/public/html/resume.html'))
-app.get('/login', (req, res) => res.sendFile(__dirname + '/public/html/login.html'))
-app.get('/signup', (req, res) => res.sendFile(__dirname + '/public/html/signup.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
